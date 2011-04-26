235bb93bd2647ec5ea5aeda88498886db495329f
Dan Williams <dan.j.williams@intel.com>
2011-04-26 13:19:53 -0700

    isci: allow fallback to option-rom if efi variable retrieval fails
    If the scu efi driver is disabled but the option-rom is enabled (during an efi
    boot) allow the code to fallback to scanning legacy option-rom space for the
    parameters.
    
    Reported-by: Yinghai Lu <yinghai.lu@oracle.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/scsi/isci/init.c
+++ drivers/scsi/isci/init.c
@@ -479,7 +479,7 @@ static int __devinit isci_pci_probe(struct pci_dev *pdev, const struct pci_devic
 	int err, i;
 	struct isci_host *isci_host;
 	const struct firmware *fw = NULL;
-	struct isci_orom *orom;
+	struct isci_orom *orom = NULL;
 	char *source = "(platform)";
 
 	check_si_rev(pdev);
@@ -491,7 +491,8 @@ static int __devinit isci_pci_probe(struct pci_dev *pdev, const struct pci_devic
 
 	if (efi_enabled)
 		orom = isci_get_efi_var(pdev);
-	else
+
+	if (!orom)
 		orom = isci_request_oprom(pdev);
 
 	for (i = 0; orom && i < ARRAY_SIZE(orom->ctrl); i++) {
