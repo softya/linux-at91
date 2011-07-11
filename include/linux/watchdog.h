d469b1647f38a8c2a08e2adf7ae2496910ea94f3
Wim Van Sebroeck <wim@iguana.be>
2011-07-11 12:59:55 +0000

    watchdog: WatchDog Timer Driver Core - Add nowayout feature
    Add support for the nowayout feature to the
    WatchDog Timer Driver Core framework.
    This feature prevents the watchdog timer from being
    stopped.
    
    Signed-off-by: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- include/linux/watchdog.h
+++ include/linux/watchdog.h
@@ -86,6 +86,7 @@ struct watchdog_device {
 #define WDOG_ACTIVE		0	/* Is the watchdog running/active */
 #define WDOG_DEV_OPEN		1	/* Opened via /dev/watchdog ? */
 #define WDOG_ALLOW_RELEASE	2	/* Did we receive the magic char ? */
+#define WDOG_NO_WAY_OUT		3	/* Is 'nowayout' feature set ? */
 };
 
 /* drivers/watchdog/core/watchdog_core.c */
